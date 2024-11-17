dc6eb0120a4561e9e104e208052c5201d8fb6ddb
euripides soto <88294617+noob339@users.noreply.github.com>
2024-11-17 19:11:04 +0000

    worked on reqs line but no avail

--- start-service.sh
+++ start-service.sh
@@ -6,6 +6,6 @@ sudo apt install graphviz
 cd /home/es4140/proj1-3/webserver
 export FLASK_ENV=development
 source ~/.virtualenvs/dbproj/bin/activate
-pip3 install -r requirements.txt
+pip3 install -r ./requirements.txt
 #flask run --host=0.0.0.0 --port=8111
 python3 server.py
